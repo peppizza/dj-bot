389c5a8ba4f348bfab9667d3fcbccbc35234bd0f
Spencer <spencervess@gmail.com>
2020-11-17 16:20:25 -0500

    cleanup remove command

--- src/commands/remove.rs
+++ src/commands/remove.rs
@@ -17,13 +17,20 @@ async fn remove(ctx: &Context, msg: &Message, mut args: Args) -> CommandResult {
     if let Some(handler_lock) = manager.get(guild_id) {
         let handler = handler_lock.lock().await;
         let queue = handler.queue();
-        if queue.dequeue(index).is_none() {
-            msg.reply(ctx, format!("Could not remove the song at {}", index))
-                .await?;
+        if !queue.is_empty() {
+            if index == 1 {
+                queue.skip()?;
+                msg.channel_id.say(ctx, "Skipped the song").await?;
+            } else if queue.dequeue(index - 1).is_none() {
+                msg.reply(ctx, format!("There is no song at {}", index))
+                    .await?;
+            } else {
+                msg.channel_id
+                    .say(ctx, format!("Removed the song at {}", index))
+                    .await?;
+            }
         } else {
-            msg.channel_id
-                .say(ctx, format!("Removed the song at {}", index))
-                .await?;
+            msg.channel_id.say(ctx, "The queue is empty").await?;
         }
     }
 
